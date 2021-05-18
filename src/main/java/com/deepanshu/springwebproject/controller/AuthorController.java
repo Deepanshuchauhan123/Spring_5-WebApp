5c9d8eb0748aebd67a170ca0802cbdd37e5e57d7
Deepanshuchauhan123 <deepanshuchauhan7287@gmail.com>
2021-05-18 20:06:29 +0530

    added author controller and thymeleaf templates

---
+++ src/main/java/com/deepanshu/springwebproject/controller/AuthorController.java
@@ -0,0 +1,22 @@
+package com.deepanshu.springwebproject.controller;
+
+import com.deepanshu.springwebproject.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping(value = "/authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors/list";
+    }
+
+}
